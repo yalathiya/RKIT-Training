9fcb686d3b32d9839ef44e20e097ea33dd57c709
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-07-31 23:06:31 +0530

    Validation & event handling script is added in registration form

--- RegistrationPageScript.js
+++ RegistrationPageScript.js
@@ -1,5 +1,5 @@
-function submitMsg() {
-  alert("Submit button clicked");
+function resetMsg() {
+  alert("Reset button clicked");
 }
 function resetMsg() {
   document.getElementById("resetMsg").innerHTML =
@@ -15,13 +15,43 @@ function assuranceMsg() {
   document.getElementById("assuranceMsg").innerHTML =
     "Dont Worry, We'll never share your information with others";
 }
-// function loadingMsg() {
-//   alert("Click OK to redirect website");
-// }
+function loadingMsg() {
+  alert("Click OK to redirect website");
+}
 function validation() {
   var f_name = document.getElementById("f_name").value;
   if (f_name.length == 0 || f_name == null) {
     alert("First Name is empty");
+    return false;
+  }
+  var l_name = document.getElementById("l_name").value;
+  if (l_name.length == 0 || l_name == null) {
+    alert("Last Name is empty");
+    return false;
+  }
+  var email = document.getElementById("email").value;
+  var atPosition = email.indexOf("@");
+  var dotPosition = email.lastIndexOf(".");
+  if (dotPosition < atPosition) {
+    alert("Please enter valid email address");
+    return false;
+  }
+  var number = document.getElementById("number").value;
+  if (isNaN(number)) {
+    alert("Please enter valid contact number..");
+    return false;
+  }
+  var pwd = document.getElementById("password").value;
+  var cnf_pwd = document.getElementById("cnf_pwd").value;
+
+  if (
+    pwd != cnf_pwd ||
+    pwd.length == 0 ||
+    pwd == null ||
+    cnf_pwd.length == 0 ||
+    cnf_pwd == null
+  ) {
+    alert("Password & Confirm Password are not same..");
     return false;
   }
 }
