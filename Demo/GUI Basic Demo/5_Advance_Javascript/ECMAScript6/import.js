8116f81b46d85d48ec368d2f4fadef825aba55a2
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-08-17 12:42:25 +0530

    restored structure

--- Demo/GUI Basic Demo/5_Advance_Javascript/ECMAScript6/import.js
+++
@@ -1,8 +0,0 @@
-import { employee1 } from "./arrowFunction.js";
-import importedFunction from "./arrowFunction.js";
-
-console.log(employee1);
-
-const array = ["e", "m", "p", "l", "o", "y", "e", "e"];
-
-array.forEach(importedFunction);
